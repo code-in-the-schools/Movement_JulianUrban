13e102c7780e58c4c474145cd3003c0208adb000
thejduman <thejduman@yahoo.com>
2020-07-29 15:40:45 +0000

    Attempt at movement

--- main.py
+++ main.py
@@ -15,14 +15,28 @@ class Character(object):
     self.y = 50
     self.hitbox = (self.x, self.y, 55, 55)
 
+    self.movex = 0
+    self.movey = 0
+
   def draw(self, surface):
     surface.blit(self.image, (self.x, self.y))
+  
+  #adding pixels to sprite position
+  def control(self, x, y): 
+    self.movex += x
+    self.movey += y
+
+  #for updating sprite position on keypress
+  def update(self):
+    self.rect.x = self.rect.x + self.movex
+    self.rect.y = self.rect.y + self.movey
 
 
 
 pygame.init()
 screen_width = 600
 screen_height = 600
+steps = 10
 screen = pygame.display.set_mode((int(screen_width),int(screen_height)))
 
 pikachu = Character()
@@ -39,3 +53,11 @@ while running:
   pikachu.draw(screen)
   pygame.display.update()
   clock.tick(60)
+
+  if event.type == pygame.KEYDOWN:
+    if event.key == pygame.K_LEFT:
+      print("Left")
+      pikachu.control(-steps,0)
+    if event.key = pygame.K_RIGHT:
+      print("Right")
+      pikachu.control(steps,0)
