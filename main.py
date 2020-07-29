2382d66b2857825f553c5599711693bd2c480b59
thejduman <thejduman@yahoo.com>
2020-07-29 15:17:42 +0000

    initial code with mr. f

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+import os
+
+img_path = os.path.join('player.png')
+
+class Character(object):
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+
+    Character.image = pygame.image.load('player.png')
+    self.image = Character.image
+    #self.image = pygame.transform.scale(self.image(50,50))
+
+    self.x = 50
+    self.y = 50
+    self.hitbox = (self.x, self.y, 55, 55)
+
+  def draw(self, surface):
+    surface.blit(self.image, (self.x, self.y))
+
+
+
+pygame.init()
+screen_width = 600
+screen_height = 600
+screen = pygame.display.set_mode((int(screen_width),int(screen_height)))
+
+pikachu = Character()
+clock = pygame.time.Clock()
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      running = False
+  
+  screen.fill((255, 255, 255))
+  pikachu.draw(screen)
+  pygame.display.update()
+  clock.tick(60)
